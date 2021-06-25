def602bfd108c1a2cc0fbb85d5a0ada9c6331c95
Matheus Azambuja <matheusazambuja35@gmail.com>
2021-06-25 16:20:02 -0300

    Changed path

---
+++ src/components/ButtonToggleTheme/index.tsx
@@ -0,0 +1,22 @@
+import { ButtonHTMLAttributes } from 'react';
+
+import { useTheme } from '../../hooks/useTheme';
+import { FaSun, FaMoon } from 'react-icons/fa'
+
+import './styles.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function ButtonToggleTheme(props: ButtonProps) {
+  const {
+    theme, toggleTheme
+  } = useTheme();
+
+  return (
+    <button className="button-theme" onClick={toggleTheme} {...props}
+      style={{ backgroundColor: 'transparent', color: theme === 'dark' ? '#FFFFFF' : '#29292e' }}
+    >
+      {theme === 'light' ? <FaMoon /> : <FaSun />}
+    </button>
+  )
+}
